320c07ced3f2e3cb3204fb3bc752e2a1bb5fab22
Kidus <mail@kidus.ca>
2024-02-12 15:21:43 -0500

    Update Pricing page

--- public/client/pricing.php
+++ public/client/pricing.php
@@ -28,27 +28,13 @@ include("header.php") ?>
                         <div class="card mb-4 p-1 rounded-0 shadow">
                             <div class="card-body">
                                 <h5 class="card-title">ID Verification &nbsp;<span class="badge badge-sm bg-dark text-light">Beta</span></h5>
-                                <h6 class="card-subtitle mb-2 text-muted"><del>$4.99</del> $3.49 One Time</h6>
+                                <h6 class="card-subtitle mb-2 text-muted"><del>CA$4.99</del> CA$3.49 One Time</h6>
                                 <ul class="list-unstyled">
                                     <li>Securely verify your photo ID</li>
                                     <li>Gain more credibility and trust</li>
                                     <li>Get a Verified badge</li>
                                 </ul>
                                 <a href="./account/badges" class="btn col-12 btn-dark rounded-0">Purchase</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="card mb-4 p-1 text-light bg-primary rounded-0 shadow">
-                            <div class="card-body">
-                                <h5 class="card-title">Credit Check &nbsp;<span class="badge badge-sm bg-light text-dark">Beta</span></h5>
-                                <h6 class="card-subtitle mb-2 text-light"><del>$9.99</del> $6.99 One Time</h6>
-                                <ul class="list-unstyled">
-                                    <li>Easy and secure credit checks</li>
-                                    <li>Get a Credit Check badge</li>   
-                                    <li>Skip ahead of other prospective tenants</li>                                        
-                                </ul>
-                                <a href="./account/badges" class="btn col-12 btn-light rounded-0">Purchase</a>
                             </div>
                         </div>
                     </div>
@@ -68,10 +54,10 @@ include("header.php") ?>
 				</div>
                 <div class="row py-5">
                     <div class="col-md-4">
-                        <div class="card mb-4 p-1 bg-bronze text-light rounded-0 shadow">
+                        <div class="card mb-4 p-1 bg-bronze border-0 text-light rounded-0 shadow">
                             <div class="card-body">
                                 <h5 class="card-title">Bronze</h5>
-                                <h6 class="card-subtitle mb-2">$2.99 One Time</h6>
+                                <h6 class="card-subtitle mb-2">CA$2.99 One Time</h6>
                                 <ul>
                                     <li>Boost your listing's visibility</li>
                                 </ul>
@@ -80,10 +66,10 @@ include("header.php") ?>
                         </div>
                     </div>
                     <div class="col-md-4">
-                        <div class="card mb-4 p-1 text-light bg-silver rounded-0 shadow">
+                        <div class="card mb-4 p-1 text-light border-0 bg-silver rounded-0 shadow">
                             <div class="card-body">
                                 <h5 class="card-title">Silver &nbsp;<span class="badge badge-sm bg-light text-dark">Best Value</span></h5>
-                                <h6 class="card-subtitle mb-2 text-light">$4.99 One Time</h6>
+                                <h6 class="card-subtitle mb-2 text-light">CA$4.99 One Time</h6>
                                 <ul>
                                     <li>Higher boost for listing visibility</li>
                                     <li>Unlock detailed listing reach statistics</li>
@@ -93,10 +79,10 @@ include("header.php") ?>
                         </div>
                     </div>
                     <div class="col-md-4">
-                        <div class="card mb-4 p-1 rounded-0 shadow bg-gold text-light">
+                        <div class="card mb-4 p-1 rounded-0 border-0 bg-gold text-light shadow">
                             <div class="card-body">
                                 <h5 class="card-title">Gold &nbsp;<span class="badge bg-dark text-light badge-sm">Best Visibility</span></h5>
-                                <h6 class="card-subtitle mb-2">$9.99 One Time</h6>
+                                <h6 class="card-subtitle mb-2">CA$9.99 One Time</h6>
                                 <ul>
                                     <li>Best possible visibility for your listing</li>
                                     <li>Everything in Bronze and Silver tiers included</li>
