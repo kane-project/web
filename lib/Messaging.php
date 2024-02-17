659fe86083e8ae382f24fc73c267c4385ccb3ecd
Kidus <mail@kidus.ca>
2024-02-17 14:43:35 -0500

    added: Message class

--- lib/Messaging.php
+++ lib/Messaging.php
@@ -4,3 +4,23 @@
 // This script handles messaging between prospective tenants and landlords
 // Author: kiduswb
 
+require_once("Database.php");
+require_once("Utils.php");
+
+class Message
+{
+    public $message_id;
+    public $sender_id;
+    public $receiver_id;
+    public $listing_id;
+    public $timestamp;
+    public $content;
+    public $is_read;
+
+    public function __construct($id = 0)
+    {
+        if ($id != 0) {
+            // Load the message from the database
+        }
+    }
+}
