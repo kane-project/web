262b8fc24f1385bc4da168065f4977dde6f9f81a
Kidus <mail@kidus.ca>
2024-04-05 11:13:41 -0400

    added: legal page scaffolding

--- public/client/legal.php
+++ public/client/legal.php
@@ -1,7 +1,10 @@
 <?php 
 
 	session_start();
-	$page = "{LegalPage}";
+	
+	if($slug == 'terms') $page = "Terms of Service";
+	if($slug == 'privacy') $page = "Privacy Policy";
+
 	include("header.php"); 
 ?>
 
@@ -16,15 +19,26 @@
 				<div class="row">
 					<div class="col-md-12 col-lg-12">
 						<div class="title-single-box">
-							<h1 class="title-single">{LegalPage}</h1>
+							<h1 class="title-single"><?php echo $page; ?></h1>
+							<?php 
+								if($page == "Terms of Service") 
+								{
+									echo <<<_END
+_END;
+								} 
+								
+								else if($page == "Privacy Policy") 
+								{
+									echo <<<_END
+_END;
+								}
+							?>
 						</div>
 					</div>
 				</div>
 			</div>
 		</section>
-
-		
-		
+			
 	</main>
 
 	<?php include("footer.php"); ?>
